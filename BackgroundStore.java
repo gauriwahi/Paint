785305b8f4636b31130cd0f3d053d9f5df8b1a51
gaurinicky <gauri_wahi998@hotmail.com>
2016-02-06 22:30:08 -0500

    Create BackgroundStore.java

---
+++ BackgroundStore.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+public class BackgroundStore {
+	
+	Color color;
+	
+	
+	public BackgroundStore(Color color){
+		this.color = color;
+	}
+	
+	//methods
+	public Color getColor(){
+		return this.color;
+	}
+	
+
+}
